73eea352dd455423992b0fde9b5b64ddfa7da955
bernardo alves <166771034+bernardoalvesc@users.noreply.github.com>
2025-04-06 12:18:35 -0300

    Update processo.h

--- processo.h
+++ processo.h
@@ -1,27 +1,27 @@
 #ifndef PROCESSO_H
 #define PROCESSO_H
 
-#include <stdio.h>
-
-#define MAX_PROCESSOS 1000 // <-- Adicione essa linha
+#define MAX_PROCESSOS 1000
+#define MAX_ASSUNTOS 10
 
 typedef struct {
     int id;
-    char numero[30];
-    char data_ajuizamento[25];
+    char numero[64];
+    char data_ajuizamento[32];
     int id_classe;
-    int id_assunto[10];
-    int qtd_assuntos;
+    int id_assunto[MAX_ASSUNTOS];
+    int num_assuntos;
     int ano_eleicao;
 } Processo;
 
-int carregar_processos(const char *nome_arquivo, Processo processos[]);
-void salvar_csv(Processo *processos, int qtd, const char *nome_arquivo);
-void ordenar_por_id(Processo *processos, int qtd);
-void ordenar_por_data(Processo *processos, int qtd);
-void contar_por_id_classe(Processo *processos, int qtd);
-void contar_id_assuntos_distintos(Processo *processos, int qtd);
-void listar_multiplos_assuntos(Processo *processos, int qtd);
-void mostrar_dias_em_tramitacao(Processo *processos, int qtd);
+int ler_processos(const char *nome_arquivo, Processo processos[]);
+void ordenar_por_id(Processo v[], int n);
+void ordenar_por_data(Processo v[], int n);
+void salvar_csv(const char *nome_arquivo, Processo v[], int n);
+int contar_por_classe(Processo v[], int n, int id_classe);
+int contar_assuntos_unicos(Processo v[], int n);
+void listar_multiplos_assuntos(Processo v[], int n);
+int dias_em_tramitacao(const char *data);
+void mostrar_dias_em_tramitacao(Processo v[], int n);
 
 #endif
